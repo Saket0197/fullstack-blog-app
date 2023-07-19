dfee4aca55be058d0c0175499da5deecc84c7d1d
Saket <saket.s9713@gmail.com>
2023-07-19 19:18:11 +0530

    modified dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xl': {'max': '1200px'},
+
+      'lg': {'max': '1024px'},
+
+      'md': {'max': '768px'},
+
+      'md2': {'max': '640px'},
+
+      'sm': {'max': '420px'},
+
+      'xs': {'max':'330px'}
+    }
   },
   plugins: [],
 }
