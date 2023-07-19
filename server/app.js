0d30885a0d35aeed34a59e6c7744da546f17a2e7
Saket <saket.s9713@gmail.com>
2023-07-19 22:50:39 +0530

    modified app.js cors origin

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ connectCloudinary();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://bloggers-blog-app.vercel.app",
     credentials:true
 }));
 
