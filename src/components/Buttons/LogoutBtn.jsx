34a050b81b1242be685052f111c05833856d25d9
Saket <saket.s9713@gmail.com>
2023-07-20 00:31:20 +0530

    updated style for Logout text

--- src/components/Buttons/LogoutBtn.jsx
+++ src/components/Buttons/LogoutBtn.jsx
@@ -8,7 +8,7 @@ export default function LogoutBtn({handleLogout,username}) {
         <div className='md:px-2 md:py-1 md:font-semibold md:text-base sm:bg-transparent capitalize rounded-md font-bold bg-[#2ed853c7] border border-[#2ed853] hover:bg-[#2ed85377] px-4 py-2 transition duration-200 flex items-center gap-2' onClick={handleLogout}>
             <BsFillGearFill className='sm:hidden'/>
             <span>Logout</span> 
-          <span className='sm:hidden font-extrabold text-[#471212d5]'>({username})</span>
+          <span className='sm:hidden font-extrabold text-[#fffc4a]'>({username})</span>
         </div>
     </NavLink>
   )
