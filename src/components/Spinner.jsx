793278ee559115a303f875c600a46aa737b179b8
Saket <saket.s9713@gmail.com>
2023-07-19 19:48:31 +0530

    added Layout and Spinner components

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../customstyles/Spinner.css';
+
+export default function Spinner() {
+  return (
+    <div className='w-90vh h-50vh flex justify-center items-center'>
+      <div className='spinner'></div>
+    </div>
+  )
+}
